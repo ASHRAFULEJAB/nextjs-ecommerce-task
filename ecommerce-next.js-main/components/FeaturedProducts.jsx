b3314888e54001c5eeae9dec6c0615329e0cf484
Ashraful Islam <19103189@iubat.edu>
2023-10-04 00:58:00 +0600

    mongose set up and featured products show

--- ecommerce-next.js-main/components/FeaturedProducts.jsx
+++ ecommerce-next.js-main/components/FeaturedProducts.jsx
@@ -1,19 +1,19 @@
-import React from 'react'
-import ProductItem from './ProductItem'
+import React from "react";
+import ProductItem from "./ProductItem";
 
 const FeaturedProducts = ({ products }) => {
   return (
-    <div className='h-full w-full flex justify-center mt-[20rem] '>
-      <div className='h-full w-10/12 mx-auto'>
-        <h2 className='mb-8 text-3xl text-[#333]'>Featured Products</h2>
+    <div className="h-full w-full flex justify-center mt-[20rem] ">
+      <div className="h-full w-10/12 mx-auto">
+        <h2 className="mb-8 text-3xl text-[#333]">Featured Products</h2>
         <div className="grid grid-cols-3 gap-16">
-           {products?.map((product) => (
-            <ProductItem key={product._id} product={product}/>
-           ))}
+          {products?.map((product) => (
+            <ProductItem key={product._id} product={product} />
+          ))}
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default FeaturedProducts
+export default FeaturedProducts;
--- ecommerce-next.js-main/src/pages/success.js
+++ ecommerce-next.js-main/src/pages/success.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Success = () => {
   return (
-    <div className="text-center mt-12 text-3xl">You have successfully payed with card!</div>
-  )
-}
+    <div className="text-center mt-12 text-3xl">
+      You have successfully payed with card!
+    </div>
+  );
+};
 
-export default Success
+export default Success;
